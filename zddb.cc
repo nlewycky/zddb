d478916576a4b74199bc97be8f4aa6a14fe98c7c
Nick Lewycky <nicholas@mxc.ca>
2024-12-16 15:14:48 -0800

    Add performance improvement for the one-element union case.
    This happens during the recursive step.
    
    Change multiunion to take a ZddNodes& instead of a Zdd. It doesn't need a root node index for the output it's building.
    
    Also add comments to improve understanding a bit.

--- zddb.cc
+++ zddb.cc
@@ -74,10 +74,13 @@ struct ZddNode {
   LabelTy label = -3;
 };
 
+// This is a container that holds any number of ZDDs that share the same set of
+// underlying variables, and can only be mutated by adding new nodes.
 struct ZddNodes {
   std::vector<ZddNode> nodes;
   std::unordered_map<std::tuple<IdxTy, IdxTy, LabelTy>, IdxTy> unique;
 
+  // Get the label for the given node by index.
   LabelTy get_label(IdxTy idx) const {
     if (idx == lo_idx)
       return LO;
@@ -87,11 +90,13 @@ struct ZddNodes {
     return nodes[idx].label;
   }
 
+  // Look up the node by index.
   const ZddNode &operator[](IdxTy idx) const {
     assert(idx < nodes.size());
     return nodes[idx];
   }
 
+  // Create a new node, or return the unique one if it already exists.
   IdxTy get(IdxTy lo, IdxTy hi, LabelTy label) {
     assert(label != LO && label != HI);
     assert(hi != lo_idx);
@@ -105,6 +110,9 @@ struct ZddNodes {
   }
 };
 
+// This is a thin wrapper around ZddNodes that represents a single specific ZDD
+// by pointing at the underlying flat array of nodes and knowing the index of
+// the root node.
 struct Zdd {
   explicit Zdd() {}
   explicit Zdd(ZddNodes *memory) : memory(memory), root(lo_idx) {}
@@ -139,8 +147,11 @@ struct Zdd {
   }
 };
 
-IdxTy multiunion(Zdd &ret, std::vector<Zdd> worklist, bool include_hi);
-IdxTy multiunion(Zdd &ret, const std::vector<Zdd> &in) {
+IdxTy multiunion(ZddNodes &ret, std::vector<Zdd> worklist, bool include_hi);
+
+// Unions any number of ZDDs. Commonly used to construct ZDDs by unioning sets.
+// Adds the nodes
+IdxTy multiunion(ZddNodes &ret, const std::vector<Zdd> &in) {
   std::vector<Zdd> worklist;
   bool include_hi = false;
   for (int i = 0, e = in.size(); i != e; ++i) {
@@ -155,7 +166,9 @@ IdxTy multiunion(Zdd &ret, const std::vector<Zdd> &in) {
   return multiunion(ret, worklist, include_hi);
 }
 
-IdxTy multiunion(Zdd &ret, std::vector<Zdd> worklist, bool include_hi) {
+// Internal API for multiunion. Worklist must not include any lo_idx or hi_idx
+// ZDDs. If you want to union with hi_idx, set include_hi instead.
+IdxTy multiunion(ZddNodes &ret, std::vector<Zdd> worklist, bool include_hi) {
 #ifndef NDEBUG
   for (auto &z : worklist)
     assert(z.root >= 0);
@@ -163,6 +176,41 @@ IdxTy multiunion(Zdd &ret, std::vector<Zdd> worklist, bool include_hi) {
 
   if (worklist.empty())
     return include_hi ? hi_idx : lo_idx;
+
+  // When we're down to one ZDD, just make a copy of it.
+  //
+  // This is a performance improvement only, multiunion works correctly if you
+  // simply remove this code block.
+  if (worklist.size() == 1 && !include_hi) {
+    std::unordered_map<IdxTy, IdxTy> cache{{lo_idx, lo_idx}, {hi_idx, hi_idx}};
+    auto root = worklist[0].root;
+    if (root < 0)
+      return root;
+    auto memory = worklist[0].memory;
+    std::vector<IdxTy> copy_worklist(1, root);
+    do {
+      auto n_idx = copy_worklist.back();
+      Zdd zdd(memory, n_idx);
+      auto lo = zdd[n_idx].lo, hi = zdd[n_idx].hi;
+      auto lo_it = cache.find(lo), hi_it = cache.find(hi);
+      bool contains_lo = lo_it != cache.end();
+      bool contains_hi = hi_it != cache.end();
+      if (contains_lo && contains_hi) {
+        auto idx = ret.get(lo_it->second, hi_it->second, zdd.get_label(n_idx));
+        if (n_idx == root)
+          return idx;
+        cache[n_idx] = idx;
+        copy_worklist.pop_back();
+      } else {
+        // Don't pop the current node, we'll revisit it after visiting the two
+        // we're pushing now, it will be a leaf next time.
+        if (!contains_lo)
+          copy_worklist.push_back(lo);
+        if (!contains_hi && lo != hi)
+          copy_worklist.push_back(hi);
+      }
+    } while (1);
+  }
 
   LabelTy next_label;
   {
@@ -213,10 +261,8 @@ IdxTy line_to_zdd(Zdd &ret, std::string line) {
   assert(line.size() < 8388608);  // assumes 32-bit 'LabelTy'
   IdxTy hi = hi_idx;
   int count = line.size() - 1;
-  for (auto i = line.rbegin(), e = line.rend(); i != e; ++i) {
-    hi = ret.get(lo_idx, hi, *i + (256 * count));
-    --count;
-  }
+  for (auto i = line.rbegin(), e = line.rend(); i != e; ++i)
+    hi = ret.get(lo_idx, hi, *i + (256 * count--));
   return hi;
 }
 
@@ -239,7 +285,7 @@ int main(int argc, char **argv) {
       }
     }
 
-    all_lines.root = multiunion(all_lines, lines);
+    all_lines.root = multiunion(flat2, lines);
     assert((all_lines.check(), true));
   }
 }
